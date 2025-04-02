62f6cd31d9ad4b8bed2c8e69f5e0cc9ef3fc82df
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-04-02 13:29:04 -0500

    feat(theme): add AppTheme class and integrate with MaterialApp
    Introduce a new AppTheme class to manage theme configurations, including a list of predefined colors and a method to generate a ThemeData object. Integrate this theme into the MaterialApp in the main.dart file to enhance UI customization and consistency.

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const List<Color> colorList = [
+  Colors.blue,
+  Colors.teal,
+  Colors.green,
+  Colors.yellow,
+  Colors.orange,
+  Colors.pink,
+  Colors.red,
+  Colors.purple,
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+    : assert(
+        selectedColor >= 0 && selectedColor < colorList.length,
+        'Colors must be between 0 and ${colorList.length}',
+      );
+
+  ThemeData theme() =>
+      ThemeData(useMaterial3: true, colorSchemeSeed: colorList[selectedColor]);
+}
