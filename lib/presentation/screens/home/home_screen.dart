8059f0f9f8d030b75e91bccb7cbc53777ba9d807
Alfredo Alonso Anchante Moreno <alonso.amoreno@live.com>
2025-04-02 13:58:55 -0500

    feat(home): add dynamic menu list to HomeScreen
    Introduce a new `_HomeView` widget that displays a list of menu items using `ListView.builder`. Each item in the list is represented by a `_CustomListTile` widget, which includes an icon, title, subtitle, and a trailing arrow. This change enhances the HomeScreen by providing a navigable menu structure, preparing for future navigation implementation.

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -1,10 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_app/config/menu/menu_items.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Flutter + Material 3')));
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter + Material 3')),
+      body: _HomeView(),
+    );
+  }
+}
+
+class _HomeView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: appMenuItems.length,
+      itemBuilder: (context, index) {
+        final menuItem = appMenuItems[index];
+
+        // Obtenemos el color primario del tema actual
+        final colors = Theme.of(context).colorScheme;
+
+        return _CustomListTile(menuItem: menuItem, colors: colors);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({required this.menuItem, required this.colors});
+
+  final MenuItems menuItem;
+  final ColorScheme colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(menuItem.icon, color: colors.primary),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subTitle),
+      trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
+      onTap: () {
+        // TODO Navegación pendiente
+      },
+    );
   }
 }
